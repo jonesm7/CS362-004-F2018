709fbb22c611a7b8a35ec1af22789d8bd9327759
Mindy Jones <cherrytomato.jones@gmail.com>
2018-10-24 16:10:45 -0700

    Exhaustive checks for all cards

--- projects/jonesmin/dominion/cardtest4.c
+++ projects/jonesmin/dominion/cardtest4.c
@@ -42,6 +42,10 @@ int main() {
   checkIntEquals(5, state1->handCount[playerIndex], "cardEffect(village): handCount");
   checkIntEquals(copper, state1->hand[playerIndex][0], "cardEffect(village): first card");
   checkIntEquals(3, state1->numActions, "cardEffect(village): numActions");
+  // check the other player's hand
+  checkIntEquals(5, state1->handCount[1], "cardEffect(adventurer): other player's hand");
+  // check supply piles that shouldn't change
+  checkCardPilesDontChange(state1, 1, 1, 1);
   
   if (!getAnyFailed()) {
     printf("TEST SUCCESSFULLY COMPLETED\n");
