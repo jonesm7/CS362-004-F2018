dcf0af74b45929e954b48fe402c0cb9ae819d564
Mindy Jones <jonesmin@oregonstate.edu>
2018-10-24 15:47:54 -0700

    Fixing compilation and test errors

--- projects/jonesmin/dominion/cardtest1.c
+++ projects/jonesmin/dominion/cardtest1.c
@@ -35,20 +35,19 @@ void checkIntEquals(int expected, int actual, char* description) {
 
 int victoryCards[] = {curse, estate, duchy, province};
 int treasureCards[] = {copper, silver, gold};
-int kingdomCards[] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
+int gameKingdomCards[] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
            sea_hag, tribute, smithy};
 
 
-void checkCardPilesDontChange(gameState* state, int checkKingdomCards, int checkVictoryCards, int checkTreasureCards) {
+void checkCardPilesDontChange(struct gameState* state, int checkKingdomCards, int checkVictoryCards, int checkTreasureCards) {
   struct gameState* initialState = malloc(sizeof(struct gameState));
-  int playerIndex = 0;
   int randomSeed = 2;
   int numPlayers = 2;
   int i;
-  initializeGame(numPlayers, kingdomCards, randomSeed, initialState);
+  initializeGame(numPlayers, gameKingdomCards, randomSeed, initialState);
   if (checkKingdomCards) {
     for (i = 0; i < 10; i++) {
-      int card = kingdomCards[i];
+      int card = gameKingdomCards[i];
       printf("Checking kingdom card %d\n", card);
       checkIntEquals(initialState->supplyCount[card], state->supplyCount[card], "Supply - kindgom card");
     }
@@ -74,7 +73,7 @@ int main() {
   int playerIndex = 0;
   int randomSeed = 2;
   int numPlayers = 2;
-  initializeGame(numPlayers, kingdomCards, randomSeed, state1);
+  initializeGame(numPlayers, gameKingdomCards, randomSeed, state1);
   int deckCount = state1->deckCount[playerIndex];
   // set top 3 cards in deck to cards we control
   state1->deck[playerIndex][deckCount-1] = baron;
@@ -85,13 +84,13 @@ int main() {
   checkIntEquals(7, state1->handCount[playerIndex], "cardEffect(smithy): handCount");
   // check top 3 cards in hand
   int handCount = state1->handCount[playerIndex];
-  checkIntEquals(baron, state1->hand[handCount-1], "cardEffect(smithy): hand[-1]");
-  checkIntEquals(great_hall, state1->hand[handCount-2], "cardEffect(smithy): hand[-2]");
-  checkIntEquals(minion, state1->hand[handCount-3], "cardEffect(smithy): hand[-3]");
+  checkIntEquals(baron, state1->hand[playerIndex][handCount-1], "cardEffect(smithy): hand[-1]");
+  checkIntEquals(great_hall, state1->hand[playerIndex][handCount-2], "cardEffect(smithy): hand[-2]");
+  checkIntEquals(minion, state1->hand[playerIndex][handCount-3], "cardEffect(smithy): hand[-3]");
   checkIntEquals(1, state1->playedCardCount, "cardEffect(smithy): played card count");
   checkIntEquals(smithy, state1->playedCards[0], "cardEffect(smithy): discarded card");
   // check the other player's hand
-  checkIntEquals(5, state1->handCount[1], "cardEffect(smithy): other player's hand");
+  checkIntEquals(0, state1->handCount[1], "cardEffect(smithy): other player's hand");
   // check supply piles that shouldn't change
   checkCardPilesDontChange(state1, 1, 1, 1);
   
