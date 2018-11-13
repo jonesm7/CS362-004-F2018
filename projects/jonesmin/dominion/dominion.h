837263e00614bed9e1f853e507f9e5451c6498ec
Mindy Jones <cherrytomato.jones@gmail.com>
2018-11-12 21:27:40 -0800

    Adding card type indexes in dominion.h

--- projects/jonesmin/dominion/dominion.h
+++ projects/jonesmin/dominion/dominion.h
@@ -23,25 +23,25 @@ enum CARD
 
    copper,
    silver,
-   gold,
+   gold, // 6
 
    adventurer,
    /* If no/only 1 treasure found, stop when full deck seen */
    council_room,
    feast, /* choice1 is supply # of card gained) */
-   gardens,
+   gardens, // 10
    mine, /* choice1 is hand# of money to trash, choice2 is supply# of
 	    money to put in hand */
    remodel, /* choice1 is hand# of card to remodel, choice2 is supply# */
    smithy,
-   village,
+   village, // 14
 
    baron, /* choice1: boolean for discard of estate */
    /* Discard is always of first (lowest index) estate */
    great_hall,
    minion, /* choice1:  1 = +2 coin, 2 = redraw */
    steward, /* choice1: 1 = +2 card, 2 = +2 coin, 3 = trash 2 (choice2,3) */
-   tribute,
+   tribute, // 19
 
    ambassador, /* choice1 = hand#, choice2 = number to return to supply */
    cutpurse,
@@ -49,7 +49,7 @@ enum CARD
    outpost,
    salvager, /* choice1 = hand# to trash */
    sea_hag,
-   treasure_map
+   treasure_map // 26
   };
 
 struct gameState {
