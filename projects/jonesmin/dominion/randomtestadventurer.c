ae9052aad436d27551282dfa5dd5edf930e164ed
Mindy Jones <cherrytomato.jones@gmail.com>
2018-11-13 22:27:08 -0800

    Removing reference code

--- projects/jonesmin/dominion/randomtestadventurer.c
+++ projects/jonesmin/dominion/randomtestadventurer.c
@@ -4,36 +4,6 @@
 #include "dominion.h"
 #include "testhelpers.h"
 
-/*
-int adventurerCardEffect(struct gameState *state, int currentPlayer) {
-          int drawntreasure=0;
-          int cardDrawn;
-          int temphand[MAX_HAND];
-          int z = 0;
-          
-
-       while(drawntreasure<2){
-        if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-          shuffle(currentPlayer, state);
-        }
-        drawCard(currentPlayer, state);
-        cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-        if (cardDrawn == copper || cardDrawn == silver)
-          drawntreasure++;
-        else{
-          temphand[z]=cardDrawn;
-          state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-          z++;
-        }
-      }
-      while(z-2 >= 0){
-        state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-        z=z-1;
-      }
-      return 0;
-}
-*/
-
 void run() {
   int kingdomCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
            sea_hag, tribute, smithy};
